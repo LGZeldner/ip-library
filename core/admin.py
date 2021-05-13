9a06335a028cd4b3c6380dec951954ccc854e1fa
LGZeldner <lgzeldner@gmail.com>
2021-05-14 00:57:06 +0300

    Admin configuration

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from core.models import Books, Book_types, Book_authors, Book_tags, Baccess, Busers, bgroups, ref_userAccess, ref_groupAccess, ref_bookAuthor, ref_bookTags
+from core.models import Books, Book_types, Book_authors, Book_tags, Baccess, Busers, bgroups, ref_userAccess, ref_userGroup, ref_groupAccess, ref_bookAuthor, ref_bookTags
 
 @admin.register(Books)
 class BooksAdmin(admin.ModelAdmin):
+    list_display = ("name", "type_id")
     pass
 
 @admin.register(Book_types)
@@ -11,6 +12,7 @@ class Book_typesAdmin(admin.ModelAdmin):
 
 @admin.register(Book_authors)
 class Book_authorsAdmin(admin.ModelAdmin):
+    list_display = ("fname", "mname", "lname")
     pass
 
 @admin.register(Book_tags)
@@ -23,6 +25,7 @@ class BaccessAdmin(admin.ModelAdmin):
 
 @admin.register(Busers)
 class BusersAdmin(admin.ModelAdmin):
+    list_display = ("name", "login", "email")
     pass
 
 @admin.register(bgroups)
@@ -31,18 +34,27 @@ class bgroupsAdmin(admin.ModelAdmin):
     
 @admin.register(ref_userAccess)
 class ref_userAccessAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "access_id")
     pass
 
+@admin.register(ref_userGroup)
+class ref_userGroupAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "group_id")
+    pass
+    
 @admin.register(ref_groupAccess)
 class ref_groupAccessAdmin(admin.ModelAdmin):
+    list_display = ("group_id", "access_id")
     pass
 
 @admin.register(ref_bookAuthor)
 class ref_bookAuthorAdmin(admin.ModelAdmin):
+    list_display = ("book_id", "author_id")
     pass
 
 @admin.register(ref_bookTags)
 class ref_bookTagsAdmin(admin.ModelAdmin):
+    list_display = ("book_id", "tag_id")
     pass
 
     
--- core/models.py
+++ core/models.py
@@ -5,58 +5,84 @@ class Books(models.Model):
     type_id = models.IntegerField()
     name = models.CharField(max_length=100)
     book = models.BinaryField()
+    def __str__(self):
+        return f"{self.type_id, self.name, self.book}"
+    class Meta:
+        ordering = ("name")
 
 # Тип книги    
 class Book_types(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name}"
 
 # Автор    
 class Book_authors(models.Model):
     lname = models.CharField(max_length=32)
     fname = models.CharField(max_length=32)
-    mname = models.CharField(max_length=32)   
+    mname = models.CharField(max_length=32)  
+    def __str__(self):
+        return f"{self.fname, self.mname, self.lname}"
   
 # Тема    
 class Book_tags(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name}"
     
 # Доступ    
 class Baccess(models.Model):
     name = models.CharField(max_length=64)
+    def __str__(self):
+        return f"{self.name}"
     
 # Пользователи    
 class Busers(models.Model):
     name = models.CharField(max_length=100)
     login = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)    
+    email = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name, self.login, self.email}"    
 
 # Группа    
 class bgroups(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name}"
 
 # Доступ по пользователям    
 class ref_userAccess(models.Model):
     user_id = models.IntegerField()
-    access_id = models.IntegerField()    
+    access_id = models.IntegerField()
+    def __str__(self):
+        return f"{self.user_id, self.access_id}"    
 
 # Доступ по группам    
 class ref_groupAccess(models.Model):
     group_id = models.IntegerField()
-    access_id = models.IntegerField()  
+    access_id = models.IntegerField()
+    def __str__(self):
+        return f"{self.group_id, self.access_id}"    
 
 # Доступ по группам    
 class ref_userGroup(models.Model):
     user_id = models.IntegerField()
     group_id = models.IntegerField()
+    def __str__(self):
+        return f"{self.user_id, self.group_id}"
 
 # Книги и их авторы    
 class ref_bookAuthor(models.Model):
     book_id = models.IntegerField()
     author_id = models.IntegerField()
+    def __str__(self):
+        return f"{self.book_id, self.author_id}"
 
 # Книги и их темы    
 class ref_bookTags(models.Model):
     book_id = models.IntegerField()
-    tag_id = models.IntegerField()  
+    tag_id = models.IntegerField()
+    def __str__(self):
+        return f"{self.book_id, self.tag_id}"    
 
